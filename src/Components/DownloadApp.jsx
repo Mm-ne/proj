c6bbe148be0ff69eee9724ad8caa3c6728dbd08a
Mm-ne <abdelmoumeneboulahbal@gmail.com>
2024-03-11 20:10:37 +0100

    [UPDATE] : Download Component 100%.

--- src/Components/DownloadApp.jsx
+++ src/Components/DownloadApp.jsx
@@ -3,8 +3,17 @@ import React from 'react'
 
 export default function DownloadApp() {
   return (
-    <div className='download-container'>
-      <img src='https://i.postimg.cc/XvYjPcsJ/back.png' />
+    <div className='download-wrapper'>
+      <div className='download-container'>
+        <h3>
+        Download the free Matjar mobile
+            <br/> application
+        </h3>
+        <p>
+          Use the Matjar app and access your business anywhere, anytime, from ayn device.
+        </p>
+        <img src="https://i.postimg.cc/66g8GSYh/download-google-play.png" className='gplay-svg'/>
+      </div>
     </div>
   )
 }
